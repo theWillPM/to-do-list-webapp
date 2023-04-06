f8c3150008e2184769ea399feece21444fe5933e
elycuaton <janelyn.cuaton@gmail.com>
2023-04-06 14:59:56 -0600

    Update project1-group2.js

--- js/project1-group2.js
+++ js/project1-group2.js
@@ -5,187 +5,171 @@
 * @author: Group 2: Adam Workie, Ely Cuaton, Jeni R Villavicencio, Joefel Cepeda, Willian P Munhoz. 
 */ 
 
-// JQuery:
-$(document).ready(function () {
+$(document).ready(function() {
     let tasks = [];
     let savedLists = [];
     let openedListIndex = -1;
+    let savedTasks = [];
 
+  
     // Add task
-    $(".add-button").click(function () {
-        let task = $("#item-to-add").val().trim();
-        if (task.length > 0) {
-            tasks.push({ task: task, status: "ongoing" });
-            displayTasks();
-            $("#item-to-add").val("");
+    function addTask(taskName) {
+      const uniqueName = getUniqueTaskName(taskName, tasks);
+      tasks.push({ task: uniqueName, status: "ongoing" });
+      displayTasks();
+    }
+  
+    function getUniqueTaskName(name, tasks) {
+      let baseName = name;
+      let counter = 1;
+      let unique = false;
+      while (!unique) {
+        unique = true;
+        for (let i = 0; i < tasks.length; i++) {
+          if (tasks[i].task === name) {
+            unique = false;
+            name = baseName + " (" + counter + ")";
+            counter++;
+            break;
+          }
         }
-    });
+      }
+      return name;
+    }
 
-    // Save list
-    $("#save-list-button").click(function () {
-        let listName = $("#current-list-name").val().trim();
-        if (listName.length === 0) {
-            listName = "Unnamed List";
-        }
-        if (openedListIndex === -1) {
-            // Save a new list
-            listName = getUniqueListName(listName);
-            savedLists.push({ name: listName, tasks: tasks.slice() });
-        } else {
-            // Update the opened list
-            listName = getUniqueListName(listName, openedListIndex);
-            savedLists[openedListIndex] = { name: listName, tasks: tasks.slice() };
-        }
-        displaySavedLists();
-        tasks = [];
-        displayTasks();
-        $("#current-list-name").val("");
-        openedListIndex = -1;
-    });
-    
-    // Display tasks
-    function displayTasks() {
-        $("#current-list").empty();
-
-        tasks.forEach(function (task, index) {
-            let taskElement = $("<li>")
-                .addClass("list-item")
-                .text(task.task)
-                .click(function () {
-                    updateStatus(index);
-                });
-            if (task.status === "complete") {
-                taskElement.css("text-decoration", "line-through");
-                taskElement.css("text-decoration-color", "green");
-                taskElement.css("color", "#888");
-                
+    $(".add-button").click(function() {
+      let task = $("#item-to-add").val().trim();
+      if (task.length > 0) {
+        const uniqueTaskName = getUniqueTaskName(task, tasks);
+        addTask(uniqueTaskName);
+        $("#item-to-add").val("");
+        $.ajax({
+          url: 'http://localhost:3000/tasks',
+          type: 'POST',
+          data: JSON.stringify({ task: uniqueTaskName, status: 'ongoing' }),
+          contentType: 'application/json',
+          success: function(response) {
+            if (response.message) { // Check if there's a message in the response
+              getTasks();
+              $("#item-to-add").val("");
+            } else {
+              console.error(response.message);
             }
-            let removeButton = $("<img class=\"remove-icon\" src=\"./img/red-dash.png\">")
-            
-                .click(function () {
-                    removeTask(index);
-                });
-            taskElement.append(removeButton);
-            $("#current-list").append(taskElement);
+          },
+          error: function(error) {
+            console.error('Error:', error);
+          },
         });
+      }
+    });
+
+    function getTasks() {
+      $("#current-list").empty();
+  
+      $.ajax({
+        url: 'http://localhost:3000/tasks',
+        type: 'GET',
+        success: function(response) {
+          console.log('Fetched tasks:', response); 
+          if (response.length > 0) { // Check if there are tasks in the response
+            tasks = response;
+            displayTasks();
+          } else {
+            console.error("No tasks found");
+          }
+        },
+        error: function(error) {
+          console.error('Error:', error);
+        },
+      });
     }
-
-
     // Remove task
-    function removeTask(index) {
-        tasks.splice(index, 1);
-        displayTasks();
-    }
-
-
+function removeTask(index) {
+  const taskId = tasks[index].task_id;
+  $.ajax({
+    url: `http://localhost:3000/tasks/${taskId}`,
+    type: 'DELETE',
+    success: function(response) {
+      if (response.message) { // Check if there's a message in the response
+        getTasks();
+      } else {
+        console.error(response.message);
+      }
+    },
+    error: function(error) {
+      console.error('Error:', error);
+    },
+  });
+}
+  
     // Update status
     function updateStatus(index) {
-        tasks[index].status = tasks[index].status === "ongoing" ? "complete" : "ongoing";
-        displayTasks();
+      const taskId = tasks[index].task_id;
+      const newStatus = tasks[index].status === 'ongoing' ? 'complete' : 'ongoing';
+    
+      $.ajax({
+        url: `http://localhost:3000/tasks/${taskId}`,
+        type: 'PUT',
+        data: JSON.stringify({ status: newStatus }),
+        contentType: 'application/json',
+        success: function (response) {
+          if (response.success) {
+            getTasks();
+          } else {
+            console.error(response.message);
+          }
+        },
+        error: function (error) {
+          console.error('Error:', error);
+        },
+      }).done(function () {
+        getTasks();
+      });
     }
-
-    // Get unique list name
-    function getUniqueListName(name, excludeIndex) {
-        let baseName = name;
-        let counter = 1;
-        let unique = false;
-        while (!unique) {
-            unique = true;
-            for (let i = 0; i < savedLists.length; i++) {
-                if (i === excludeIndex) {
-                    continue;
-                }
-                if (savedLists[i].name === name) {
-                    unique = false;
-                    name = baseName + " (" + counter + ")";
-                    counter++;
-                    break;
-                }
-            }
+    
+    // Call getTasks() when the page is loaded
+      getTasks();
+    
+  
+    // Display tasks
+    function displayTasks() {
+      $("#current-list").empty();
+    
+      tasks.forEach(function (task, index) {
+        let taskElement = $("<li>")
+          .addClass("list-item")
+          .text(task.task)
+          .on("click", function () { // Change this line
+            updateStatus(index);
+          });
+        if (task.status === "complete") {
+          taskElement.css("text-decoration", "line-through");
+          taskElement.css("text-decoration-color", "green");
+          taskElement.css("color", "#888");
         }
-        return name;
-    }
+        let removeButton = $("<img class=\"remove-icon\" src=\"./img/red-dash.png\">")
+          .on("click", function (event) { // Change this line
+            event.stopPropagation();
+            removeTask(index);
+          });
+        taskElement.append(removeButton);
+        $("#current-list").append(taskElement);
+      });
+    } 
+  });
     
-    // Display saved lists
-    function displaySavedLists() {
-        $("#list-of-saved-lists").empty();
-        savedLists.forEach(function (list, index) {
-            let listElement = $("<li>")
-                .addClass("saved-list-title")
-                .text(list.name)
-                .click(function () {
-                    tasks = list.tasks.slice();
-                    displayTasks();
-                    openedListIndex = index;
-                    $("#current-list-name").val(list.name);
-                });
-            let removeButton = $("<input>")
-                .attr("type", "button")
-                .addClass("remove-button")
-                .val("-")
-                .click(function (event) {
-                    event.stopPropagation();
-                    removeSavedList(index);
-                });
-            listElement.append(removeButton);
-            $("#list-of-saved-lists").append(listElement);
-        });
-    }
-
-    // Remove saved list
-    function removeSavedList(index) {
-        savedLists.splice(index, 1);
-        displaySavedLists();
-    }
-    
-    // Delete all items in the current list
-    $("#delete-all-items-button").click(function () {
-    if (confirm("Are you sure you want to delete all items in the current list?")) {
-        deleteAllItems();
-        displayTasks();
-    }
-    });
-
-    // Delete all saved lists
-    $("#delete-all-lists-button").click(function() {
-    if (confirm("Are you sure you want to delete all saved lists?")) {
-        deleteAllLists();
-        displaySavedLists();
-    }
-    });
-
-
-    function deleteAllItems() {
-    tasks.length = 0;
-    }
-
-    function deleteAllLists() {
-    savedLists.length = 0;
-    }
-
 // Detect if user is in Dark Mode and automatically apply it:
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     DarkTheme();
 }
-    
-});
 
 // Functions to allow using ENTER in input fields to complete the action:
-    //List Name
-    var listNameInput = document.getElementById("current-list-name");
-    listNameInput.addEventListener("keypress", function(e) {
-        if (e.key === "Enter") {
-            e.preventDefault();
-            $("#save-list-button").click();
-        }
-    });
-
     //Item
     var itemName = document.getElementById("item-to-add");
     itemName.addEventListener("keypress", function(e) {
         if (e.key === "Enter") {
             e.preventDefault();
-            $("#item-add-button").click();
+            $(".add-button").click();
         }
     });
 //
@@ -203,5 +187,9 @@ function LightTheme() {
     $("#dark-theme-icon").attr("src", "./img/dark-icon.png");
     $("#light-theme-icon").attr("src", "./img/light-icon.png");
 }
+
+
+
+
 
 
