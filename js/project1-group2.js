dc7bdc2439eb203706be952963eed5d4bb711da4
elycuaton <janelyn.cuaton@gmail.com>
2023-04-10 20:03:48 -0600

    adding server.js, modifying project1-group2.js, slight changes in css and html

--- js/project1-group2.js
+++ js/project1-group2.js
@@ -3,205 +3,386 @@
 * @Course Code: SODV1201 
 * @class: Software Development Diploma program. 
 * @author: Group 2: Adam Workie, Ely Cuaton, Jeni R Villavicencio, Joefel Cepeda, Willian P Munhoz. 
-*/ 
+*/
 
-// JQuery:
 $(document).ready(function () {
-    let tasks = [];
-    let savedLists = [];
-    let openedListIndex = -1;
-
+    savedLists = [];
+    openedListIndex = -1;
+    savedTasks = [];
+    tasks = [];
+  
+    
     // Add task
+    function addTask(taskName) {
+      const uniqueName = getUniqueTaskName(taskName, tasks);
+      tasks.push({ task: uniqueName, status: "ongoing" });
+      displayTasks();
+    }
+  
+    function getUniqueTaskName(name, tasks) {
+      let baseName = name;
+      let counter = 1;
+      let unique = false;
+      while (!unique) {
+        unique = true;
+        for (let i = 0; i < tasks.length; i++) {
+          if (tasks[i].task === name) {
+            unique = false;
+            name = baseName + " (" + counter + ")";
+            counter++;
+            break;
+          }
+        }
+      }
+      return name;
+    }
+  
+  
     $(".add-button").click(function () {
-        let task = $("#item-to-add").val().trim();
-        if (task.length > 0) {
-            tasks.push({ task: task, status: "ongoing" });
-            displayTasks();
-            $("#item-to-add").val("");
-        }
-    });
-
-    // Save list
-    $("#save-list-button").click(function () {
-        let listName = $("#current-list-name").val().trim();
-        if (listName.length === 0) {
-            listName = "Unnamed List";
-        }
-        if (openedListIndex === -1) {
-            // Save a new list
-            listName = getUniqueListName(listName);
-            savedLists.push({ name: listName, tasks: tasks.slice() });
-        } else {
-            // Update the opened list
-            listName = getUniqueListName(listName, openedListIndex);
-            savedLists[openedListIndex] = { name: listName, tasks: tasks.slice() };
-        }
-        displaySavedLists();
-        tasks = [];
-        displayTasks();
-        $("#current-list-name").val("");
-        openedListIndex = -1;
+      let task = $("#task-to-add").val().trim();
+      if (task.length > 0) {
+        const uniqueTaskName = getUniqueTaskName(task, tasks);
+        addTask(uniqueTaskName);
+        $("#task-to-add").val("");
+        $.ajax({
+          url: 'http://localhost:3000/tasks',
+          type: 'POST',
+          data: JSON.stringify({ task: uniqueTaskName, status: 'ongoing' }),
+          contentType: 'application/json;  charset=utf-8',
+          success: function (response) {
+            if (response.message) { 
+              if (openedListIndex !== -1) { 
+                getTasks();
+              }
+              $("#task-to-add").val("");
+            } else {
+              console.error(response.message);
+            }
+          },
+          error: function (error) {
+            console.error('Error:', error);
+          },
+        });
+      }
     });
     
-    // Display tasks
-    function displayTasks() {
+    function getTasks() {
+      if (openedListIndex !== -1) {
         $("#current-list").empty();
-
-        tasks.forEach(function (task, index) {
-            let taskElement = $("<li>")
-                .addClass("list-item")
-                .text(task.task)
-                .click(function () {
-                    updateStatus(index);
-                });
-            if (task.status === "complete") {
-                taskElement.css("text-decoration", "line-through");
-                taskElement.css("text-decoration-color", "green");
-                taskElement.css("color", "#888");
-                
+    
+        $.ajax({
+          url: `http://localhost:3000/lists/${savedLists[openedListIndex].list_id}/tasks`,
+          type: 'GET',
+          success: function (response) {
+            console.log('Fetched tasks:', response);
+            if (response.length > 0) { 
+              tasks = response;
+              displayTasks();
+            } else {
+              console.error("No tasks found");
             }
-            let removeButton = $("<img class=\"remove-icon\" src=\"./img/red-dash.png\">")
-            
-                .click(function () {
-                    removeTask(index);
-                });
-            taskElement.append(removeButton);
-            $("#current-list").append(taskElement);
+          },
+          error: function (error) {
+            console.error('Error:', error);
+          },
         });
+      }
     }
-
-
+    
     // Remove task
     function removeTask(index) {
-        tasks.splice(index, 1);
-        displayTasks();
+      const taskId = tasks[index].task_id;
+      $.ajax({
+        url: `http://localhost:3000/tasks/${taskId}`,
+        type: 'DELETE',
+        success: function (response) {
+          if (response.message) { 
+            getTasks();
+          } else {
+            console.error(response.message);
+          }
+        },
+        error: function (error) {
+          console.error('Error:', error);
+        },
+      });
     }
-
-
+  
     // Update status
     function updateStatus(index) {
-        tasks[index].status = tasks[index].status === "ongoing" ? "complete" : "ongoing";
-        displayTasks();
+      const taskId = tasks[index].task_id;
+      const newStatus = tasks[index].status === 'ongoing' ? 'complete' : 'ongoing';
+  
+      $.ajax({
+        url: `http://localhost:3000/tasks/${taskId}`,
+        type: 'PUT',
+        data: JSON.stringify({ status: newStatus }),
+        contentType: 'application/json',
+        success: function (response) {
+          if (response.success) {
+            getTasks();
+          } else {
+            console.error(response.message);
+          }
+        },
+        error: function (error) {
+          console.error('Error:', error);
+        },
+      }).done(function () {
+        getTasks();
+      });
     }
-
+  
+  
+    function saveList() {
+      const listName = $("#current-list-name").val();
+      if (listName.trim() === "") {
+        alert("Please enter a name for the list.");
+        return;
+      }
+      const data = { list: listName, tasks: tasks };
+      $.ajax({
+        url: "http://localhost:3000/lists",
+        type: "POST",
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+          console.log(response);
+          savedLists.push({ name: listName, list_id: response.insertId, tasks: [] }); // Create a new tasks array for the new list
+          tasks = []; // Clear all the tasks
+          displayTasks();
+          fetchAndDisplaySavedLists(); // Fetch and display saved lists after saving the new list
+        },
+        error: function (error) {
+          console.error("Error:", error);
+        },
+      });
+    }
+    
+    
+  
+  // Display tasks
+  function displayTasks() {
+    const taskListElement = document.getElementById("current-list");
+    taskListElement.innerHTML = ''; // Clear the existing tasks
+  
+    tasks.forEach((task, index) => {
+      let taskElement = $("<li>")
+        .addClass("list-item")
+        .text(task.task)
+        .on("click", function () {
+          updateStatus(index);
+        });
+      if (task.status === "complete") {
+        taskElement.css("text-decoration", "line-through");
+        taskElement.css("text-decoration-color", "green");
+        taskElement.css("color", "#888");
+      }
+      let removeButton = $("<img class=\"remove-icon\" src=\"./img/red-dash.png\">")
+        .on("click", function (event) {
+          event.stopPropagation();
+          removeTask(index);
+        });
+      taskElement.append(removeButton);
+      $("#current-list").append(taskElement);
+    });
+  }
+  
+  
+  
+  
+    // Save list
+  $("#save-list-button").click(function () {
+    let listName = $("#current-list-name").val().trim();
+    if (listName.length === 0) {
+      listName = "Unnamed List";
+    }
+  
+    if (openedListIndex === -1) {
+      // Save a new list
+      listName = getUniqueListName(listName);
+      savedLists.push({ name: listName, tasks: tasks.slice() });
+  
+      // Clear the tasks array and update the display for the new list
+      tasks = [];
+      displayTasks();
+    } else {
+      // Update the opened list
+      listName = getUniqueListName(listName, openedListIndex);
+      savedLists[openedListIndex] = { name: listName, tasks: tasks.slice() };
+    }
+  
+    saveList(listName);
+  
+    $("#current-list-name").val("");
+    openedListIndex = -1;
+    fetchAndDisplaySavedLists(); // Call this function to fetch and display the latest saved lists
+  });
+  
+  
+  
+  function fetchAndDisplaySavedLists() {
+    $.ajax({
+      url: 'http://localhost:3000/lists',
+      type: 'GET',
+      success: function (response) {
+        console.log('Fetched saved lists:', response);
+        if (response.length > 0) {
+          savedLists = response;
+          displaySavedLists();
+        } else {
+          console.error("No saved lists found");
+        }
+      },
+      error: function (error) {
+        console.error('Error:', error);
+      },
+    });
+  }
+  
     // Get unique list name
     function getUniqueListName(name, excludeIndex) {
-        let baseName = name;
-        let counter = 1;
-        let unique = false;
-        while (!unique) {
-            unique = true;
-            for (let i = 0; i < savedLists.length; i++) {
-                if (i === excludeIndex) {
-                    continue;
-                }
-                if (savedLists[i].name === name) {
-                    unique = false;
-                    name = baseName + " (" + counter + ")";
-                    counter++;
-                    break;
-                }
-            }
+      let baseName = name;
+      let counter = 1;
+      let unique = false;
+      while (!unique) {
+        unique = true;
+        for (let i = 0; i < savedLists.length; i++) {
+          if (i === excludeIndex) {
+            continue;
+          }
+          if (savedLists[i].name === name) {
+            unique = false;
+            name = baseName + " (" + counter + ")";
+            counter++;
+            break;
+          }
         }
-        return name;
+      }
+      return name;
+    }
+  
+    /* LISTS */
+    // Remove saved list
+    function removeSavedList(list) {
+      const index = savedLists.findIndex(x => x.name === list);
+      if (index === -1) {
+        console.error(`List '${list}' not found in savedLists`);
+        return;
+      }
+      const listId = savedLists[index].list_id;
+    
+      $.ajax({
+        url: `http://localhost:3000/lists/${listId}`,
+        type: 'DELETE',
+        success: function (response) {
+          if (response.message) {
+            displaySavedLists();
+          } else {
+            console.error(response.message);
+          }
+        },
+        error: function (xhr, status, error) {
+          console.error(`Error removing list '${savedLists[index].name}':`, error);
+        },
+      });
+    
+      savedLists.splice(index, 1);
+      displaySavedLists();
     }
     
-    // Display saved lists
+  
+  
     function displaySavedLists() {
-        $("#list-of-saved-lists").empty();
-        savedLists.forEach(function (list, index) {
+      $.ajax({
+        url: 'http://localhost:3000/lists',
+        type: 'GET',
+        contentType: 'application/json; charset=utf-8',
+        success: function (response) {
+          // Update the savedLists array with the fetched lists
+          savedLists = response;
+    
+          // Clear the existing list items
+          $('#list-of-saved-lists').empty();
+    
+          // Iterate through the response and display the lists
+          response.forEach(function (list, index) {
             let listElement = $("<li>")
-                .addClass("saved-list-title")
-                .text(list.name)
-                .click(function () {
-                    tasks = list.tasks.slice();
-                    displayTasks();
-                    openedListIndex = index;
-                    $("#current-list-name").val(list.name);
-                });
+              .addClass("saved-list-title")
+              .text(list.list)
+              //.click(function () {
+                listElement.click(function () {
+                  $.ajax({
+                      url: `http://localhost:3000/lists/${list.list_id}/tasks`,
+                      type: 'GET',
+                      success: function (tasksResponse) {
+                        tasks = tasksResponse;
+                        displayTasks(); // Display the fetched tasks
+                      },
+                      error: function (error) {
+                        console.error('Error fetching tasks for list:', error);
+                      }
+                  });
+              
+                  openedListIndex = index;
+                  $("#current-list-name").val(list.list);
+              });         
             let removeButton = $("<input>")
-                .attr("type", "button")
-                .addClass("remove-button")
-                .val("-")
-                .click(function (event) {
-                    event.stopPropagation();
-                    removeSavedList(index);
-                });
+              .attr("type", "button")
+              .addClass("remove-button")
+              .val("-")
+              .click(function (event) {
+                event.stopPropagation();
+                removeSavedList(savedLists[index].name);
+              });
             listElement.append(removeButton);
             $("#list-of-saved-lists").append(listElement);
-        });
-    }
-
-    // Remove saved list
-    function removeSavedList(index) {
-        savedLists.splice(index, 1);
-        displaySavedLists();
+          });
+        },
+        error: function (error) {
+          console.error('Error:', error);
+        },
+      });
     }
     
-    // Delete all items in the current list
-    $("#delete-all-items-button").click(function () {
-    if (confirm("Are you sure you want to delete all items in the current list?")) {
-        deleteAllItems();
-        displayTasks();
-    }
-    });
-
-    // Delete all saved lists
-    $("#delete-all-lists-button").click(function() {
-    if (confirm("Are you sure you want to delete all saved lists?")) {
-        deleteAllLists();
-        displaySavedLists();
-    }
-    });
-
-
-    function deleteAllItems() {
-    tasks.length = 0;
-    }
-
-    function deleteAllLists() {
-    savedLists.length = 0;
-    }
-
-// Detect if user is in Dark Mode and automatically apply it:
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  
+    displaySavedLists();
+  });
+  
+  // Detect if user is in Dark Mode and automatically apply it:
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     DarkTheme();
-}
-    
-});
-
-// Functions to allow using ENTER in input fields to complete the action:
-    //List Name
-    var listNameInput = document.getElementById("current-list-name");
-    listNameInput.addEventListener("keypress", function(e) {
-        if (e.key === "Enter") {
-            e.preventDefault();
-            $("#save-list-button").click();
-        }
-    });
-
-    //Item
-    var itemName = document.getElementById("item-to-add");
-    itemName.addEventListener("keypress", function(e) {
-        if (e.key === "Enter") {
-            e.preventDefault();
-            $("#item-add-button").click();
-        }
-    });
-//
-
-function DarkTheme() {
+  }
+  
+  // Functions to allow using ENTER in input fields to complete the action:
+  //Item
+  var itemName = document.getElementById("task-to-add");
+  itemName.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      $(".add-button").click();
+    }
+  });
+  //
+  
+  function DarkTheme() {
     console.log("Changed theme to Dark");
     $("#css-link").attr("href", "./css/dark_theme.css");
     $("#dark-theme-icon").attr("src", "./img/dark-icon-white.png");
     $("#light-theme-icon").attr("src", "./img/light-icon-white.png");
-}
-
-function LightTheme() {
+  }
+  
+  function LightTheme() {
     console.log("Changed theme to Light");
     $("#css-link").attr("href", "./css/light_theme.css");
     $("#dark-theme-icon").attr("src", "./img/dark-icon.png");
     $("#light-theme-icon").attr("src", "./img/light-icon.png");
-}
-
-
+  }
+  
+  
+  
+  
+  
+  
+  
---
+++ js/server.js
@@ -0,0 +1,275 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors({
+  origin: 'http://localhost:52330',
+  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept'
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let connection; // make connection a global variable
+
+async function setupConnection() {
+  try {
+    connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '01251999',
+      multipleStatements: true
+    });
+
+    await connection.query('CREATE DATABASE IF NOT EXISTS todo_list;');
+    await connection.query('USE todo_list;');
+    await connection.query('CREATE TABLE IF NOT EXISTS `tasks` (`task_id` int NOT NULL AUTO_INCREMENT, `task` varchar(45) DEFAULT NULL, `status` varchar(45) DEFAULT NULL, `list_id` int DEFAULT NULL, PRIMARY KEY (`task_id`));');
+    await connection.query('CREATE TABLE IF NOT EXISTS `lists` (`list_id` int NOT NULL AUTO_INCREMENT, `list` varchar(45) DEFAULT NULL, PRIMARY KEY (`list_id`));');
+
+    console.log('Connected to database and tables created!');
+  } catch (error) {
+    console.error('Error connecting to database:', error);
+    throw error;
+  }
+}
+
+// Call setupConnection to initialize the connection and set up the tables
+setupConnection();
+
+
+async function fetchSavedLists(connection) {
+  const [savedLists] = await connection.query(`
+    SELECT lists.list_id, lists.list, tasks.task_id, tasks.task, tasks.status
+    FROM lists
+    LEFT JOIN tasks ON tasks.list_id = lists.list_id
+    ORDER BY lists.list_id, tasks.task_id;
+  `);
+
+  const groupedLists = savedLists.reduce((result, row) => {
+    if (!result[row.list_id]) {
+      result[row.list_id] = {
+        list_id: row.list_id,
+        list: row.list,
+        tasks: [],
+      };
+    }
+
+    if (row.task_id) {
+      result[row.list_id].tasks.push({
+        task_id: row.task_id,
+        task: row.task,
+        status: row.status,
+      });
+    }
+
+    return result;
+  }, {});
+
+  return Object.values(groupedLists);
+}
+
+async function getUniqueTaskName(name, connection) {
+  let baseName = name;
+  let counter = 1;
+  let unique = false;
+
+  while (!unique) {
+    unique = true;
+    const [rows] = await connection.query('SELECT * FROM tasks WHERE task = ?', [name]);
+
+    if (rows.length > 0) {
+      unique = false;
+      name = baseName + " (" + counter + ")";
+      counter++;
+    }
+  }
+  return name;
+}
+async function getUniqueListName(name, connection) {
+  let baseName = name;
+  let counter = 1;
+  let unique = false;
+
+  while (!unique) {
+    unique = true;
+    const [rows] = await connection.query('SELECT * FROM lists WHERE list = ?', [name]);
+
+    if (rows.length > 0) {
+      unique = false;
+      name = baseName + " (" + counter + ")";
+      counter++;
+    }
+  }
+  return name;
+}
+
+/* SELECT queries */
+  // select tasks and list
+  app.get('/saved-lists', async (req, res) => {
+    try {
+      const savedLists = await fetchSavedLists(connection);
+      res.status(200).json(savedLists);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error fetching saved lists and tasks' });
+    }
+  });
+  
+  
+
+  // Select all tasks
+  app.get('/tasks', async (req, res) => {
+    try {
+      const [rows, fields] = await connection.query('SELECT * FROM tasks');
+      res.json(rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error getting tasks from database!' });
+    }
+  });
+
+  // Select all list
+  app.get('/lists', async (req, res) => {
+    try {
+      const [rows, fields] = await connection.query('SELECT * FROM lists');
+      res.json(rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error getting lists from database!' });
+    }
+  });
+
+  // Select list name by ID
+app.get('/lists/:id', async (req, res) => {
+  try {
+    const listId = req.params.id;
+    const [rows, fields] = await connection.query('SELECT list FROM lists WHERE list_id = ?', [listId]);
+    res.json(rows[0].list);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error getting list name from database!' });
+  }
+});
+
+// Select tasks by list ID
+app.get('/lists/:id/tasks', async (req, res) => {
+  try {
+    const listId = req.params.id;
+    const [rows, fields] = await connection.query('SELECT * FROM tasks WHERE list_id = ?', [listId]);
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error getting tasks for the specified list from the database!' });
+  }
+});
+
+
+  
+
+/* INSERT queries */
+  // Insert task
+  app.post('/tasks', async (req, res) => {
+    try {
+      const task = req.body.task;
+      const status = req.body.status;
+      const uniqueTaskName = await getUniqueTaskName(task, connection);
+  
+      await connection.query('INSERT INTO tasks (task, status) VALUES (?, ?)', [uniqueTaskName, status]);
+  
+      const savedLists = await fetchSavedLists(connection);
+      res.status(200).json({ message: 'Task added to database!', savedLists });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error adding task to database!' });
+    }
+  });
+
+  app.post('/lists', async (req, res) => {
+    try {
+      const list = req.body.list;
+      const uniqueListName = await getUniqueListName(list, connection);
+  
+      const [insertResult] = await connection.query(`INSERT INTO lists(list) VALUES (?); SELECT LAST_INSERT_ID() AS list_id;`, [list]);
+      const list_id = insertResult[1][0].list_id;
+  
+      await connection.query(`UPDATE todo_list.tasks SET list_id = ? WHERE list_id IS NULL`, [list_id]);
+  
+      const savedLists = await fetchSavedLists(connection);
+      res.status(200).json({ message: 'List added to database!', savedLists });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error adding list to database!' });
+    }
+  });
+
+  
+
+/* UPDATE queries */
+  // Change task status
+  app.put('/tasks/:taskId', async (req, res) => {
+    const taskId = req.params.taskId;
+    const status = req.body.status;
+  
+    if (!taskId || !status) {
+      return res.status(400).json({ error: 'taskId and status are required' });
+    }
+  
+    try {
+      const [result] = await connection.execute(
+        'UPDATE tasks SET status = ? WHERE task_id = ?',
+        [status, taskId]
+      );
+  
+      if (result.affectedRows === 0) {
+        return res.status(404).json({ error: 'Task not found' });
+      }
+  
+      res.json({ message: 'Task updated successfully.' });
+    } catch (error) {
+      console.error(`Error updating task: ${error.message}`);
+      res.status(500).json({ error: 'An error occurred while updating the task.' });
+    }
+  });
+  
+  
+
+/* DELETE queries */
+  // Delete task
+  app.delete('/tasks/:id', async (req, res) => {
+    try {
+      const taskId = req.params.id;
+      await connection.query('DELETE FROM tasks WHERE task_id = ?', [taskId]);
+      await fetchSavedLists(connection);
+      res.status(200).json({ message: 'Task removed from database!' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error removing task from database!' });
+    }
+  });
+  
+
+  // Delete list
+  app.delete('/lists/:id', async (req, res) => {
+    try {
+      const listId = req.params.id;
+      if (typeof listId === 'undefined') {
+        return res.status(400).json({ error: 'Missing list_id parameter' });
+      }
+      await connection.query(`DELETE FROM lists WHERE list_id = ?`, [listId]);
+      await fetchSavedLists(connection);
+      res.status(200).json({ message: 'List removed from database!' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error removing List from database!' });
+    }
+  });
+  
+/* Server Status */  
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
